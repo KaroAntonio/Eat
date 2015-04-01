693503167bc74293428894ba5be3fbff33b72e20
Robert Smiley <SmileyR@mymacewan.ca>
2015-04-01 11:25:45 -0600

    buff icons

--- EAT/Assets/Player/CamEffects.cs
+++ EAT/Assets/Player/CamEffects.cs
@@ -2,9 +2,18 @@
 using System.Collections;
 
 public class CamEffects : MonoBehaviour {
+	private GameObject speedIcon;
+	private GameObject jumpIcon;
+	private GameObject blindIcon;
+	private GameObject sizeIcon;
 
 	// Use this for initialization
 	void Start () {
+		speedIcon = GameObject.Find("SpeedIcon");
+		jumpIcon = GameObject.Find("JumpIcon");
+		blindIcon = GameObject.Find("BlindIcon");
+		sizeIcon = GameObject.Find("SizeIcon");
+
 		RenderSettings.fog = true;
 		RenderSettings.fogStartDistance = 0.0f;
 		RenderSettings.fogDensity = 0.0f;
@@ -12,7 +21,34 @@ public class CamEffects : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log(PlayerVars.buffSpeed);
+
 		RenderSettings.fogDensity = PlayerVars.buffFogIntensity;
 		RenderSettings.fogColor = PlayerVars.buffFogColor;
+
+		if(PlayerVars.buffSpeed == 1.0f){
+			speedIcon.SetActive(false);
+		} else{
+			speedIcon.SetActive(true);
+		}
+
+		if(PlayerVars.buffJump == 1.0f){
+			jumpIcon.SetActive(false);
+		} else{
+			jumpIcon.SetActive(true);
+		}
+
+		if(PlayerVars.buffFogIntensity == 0.0f){
+			blindIcon.SetActive(false);
+		} else{
+			blindIcon.SetActive(true);
+		}
+
+		if(PlayerVars.buffScale == 1.0f){
+			sizeIcon.SetActive(false);
+		} else{
+			sizeIcon.SetActive(true);
+		}
+		
 	}
 }
